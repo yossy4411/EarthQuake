9aa89273d4c00be50387177a04c434c3930540d8
yossy4411 <yaidonliving@gmail.com>
2024-08-26 21:02:13 +0900

    pbfファイルのパース

--- ConsoleTest/PerformanceTest/Program.cs
+++ ConsoleTest/PerformanceTest/Program.cs
@@ -1,41 +1,35 @@
-﻿using System.Diagnostics;
-using SkiaSharp;
+﻿using System.IO.Compression;
+using System.Threading.Channels;
+using Mapbox.VectorTile;
 
-namespace PerformanceTest;
 
-public static class Program
+using var client = new HttpClient();
+
+var file = await client.GetByteArrayAsync("http://172.18.0.93:8080/data/v3/10/899/406.pbf");
+
+
+// PBFデータを解凍する
+using var compressedStream = new MemoryStream(file);
+await using var decompressionStream = new GZipStream(compressedStream, CompressionMode.Decompress);
+using var resultStream = new MemoryStream();
+decompressionStream.CopyTo(resultStream);
+var decompressed = resultStream.ToArray();
+
+var vectorTile = new VectorTile(decompressed);
+
+var layer = vectorTile.GetLayer("landuse");
+// 1個目のフィーチャを取得
+var feature = layer.GetFeature(0);
+// ポリゴンのジオメトリを取得
+var geometry = feature.Geometry<float>();
+foreach (var point2d in geometry.SelectMany(x => x))
 {
-    private static void Main()
-    {
-        var stopwatch = new Stopwatch();
-        stopwatch.Start();
-
-        for (var i = 0; i < 100; i++)
-        {
-            using var surface = SKSurface.Create(new SKImageInfo(800, 600));
-            var canvas = surface.Canvas;
-            canvas.Clear(SKColors.White);
-
-            // 仮想的に画面外にある SKVertices
-            var vertices = SKVertices.CreateCopy(
-                SKVertexMode.TriangleFan,
-                [new SKPoint(1000, 1000), new SKPoint(1200, 1000), new SKPoint(1100, 1300)],
-                null,
-                null,
-                null
-            );
-            using var paint = new SKPaint();
-            paint.Color = SKColors.Red;
-            canvas.DrawVertices(vertices, SKBlendMode.Color, paint);
-
-            // 画像を保存するための処理など
-            using var image = surface.Snapshot();
-            using var data = image.Encode();
-            using var stream = File.OpenWrite("output.png");
-            data.SaveTo(stream);
-        }
-
-        stopwatch.Stop();
-        Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
-    }
+    Console.WriteLine(point2d.X);
+    Console.WriteLine(point2d.Y);
+    
+    // Webメルカトルの位置?に変換
+    var x = point2d.X / 4096 * 360 - 180;
+    var y = point2d.Y / 4096 * 360 - 180;
+    Console.WriteLine(x);
+    Console.WriteLine(y);
 }
