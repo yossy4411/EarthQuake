d9935897f43cdaa8c9b0404dc737f74a979aaead
yossy4411 <yossy4411.dev@gmail.com>
2024-09-07 11:49:11 +0900

    TilePointの場所の変更

--- EarthQuake.Map/Tiles/MapTilesController.cs
+++ EarthQuake.Map/Tiles/MapTilesController.cs
@@ -204,12 +204,6 @@ namespace EarthQuake.Map.Tiles
         }
         
         private record MapTileRequest(SKPoint Point, TilePoint TilePoint);
-        public readonly record struct TilePoint(int X, int Y, int Z)
-        {
-            public static readonly TilePoint Empty = new();
-            public TilePoint Add(int x, int y) => new(X + x, Y + y, Z);
-            public static TilePoint operator +(TilePoint point, (int x, int y)point1) => point.Add(point1.x, point1.y);
-        }
         
     }
     
