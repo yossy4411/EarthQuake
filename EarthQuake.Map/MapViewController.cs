881542bfa1ae54f76d1ecc7cc2ae92b32b079fc8
yossy4411 <yossy4411.dev@gmail.com>
2024-09-18 22:35:53 +0900

    描画オブジェクトをキャッシュする

--- EarthQuake.Map/MapViewController.cs
+++ EarthQuake.Map/MapViewController.cs
@@ -6,34 +6,53 @@ using SkiaSharp;
 
 namespace EarthQuake.Map;
 
-public class MapViewController()
+public class MapViewController
 {
-    private readonly MapLayer[] _mapLayers = [];
+    private readonly IEnumerable<MapLayer> _mapLayers = [];
+    private readonly IEnumerable<ICacheableLayer> cacheableLayers;
+    private SKPicture? cached;
     public MapLayer[] MapLayers
     {
-        get => _mapLayers;
         init
         {
-            _mapLayers = value; 
-            foreach (var item in _mapLayers)
+            foreach (var item in value)
             {
                 item.Update();
             }
+            cacheableLayers = value.OfType<ICacheableLayer>();
+            _mapLayers = value.Where(x => x is not ICacheableLayer);
         }
     }
 
     public void RenderBase(SKCanvas canvas, float scale, SKRect bounds)
     {
-        foreach (var layer in MapLayers)
+        if (cacheableLayers.Any(x => x.IsUpdated || x.IsReloadRequired(scale, bounds)))
+        {
+            cached?.Dispose();
+            cached = null;
+            using var recorder = new SKPictureRecorder();
+            using var c = recorder.BeginRecording(bounds);
+            foreach (var cacheableLayer in cacheableLayers)
+            {
+                cacheableLayer.IsUpdated = false;
+                cacheableLayer.Render(c, scale, bounds);
+                
+            }
+            cached = recorder.EndRecording();
+        }
+        if (cached is not null)
+        {
+            canvas.DrawPicture(cached);
+        }
+        foreach (var layer in _mapLayers)
         {
             if (layer is ForeGroundLayer) continue;
             layer.Render(canvas, scale, bounds);
         }
-
     }
     public void RenderForeGround(SKCanvas canvas, float scale, SKRect bounds, object? param = null)
     {
-        foreach (var layer in MapLayers)
+        foreach (var layer in _mapLayers)
         {
             if (layer is not ForeGroundLayer fore) continue;
             if (fore is Hypo3DViewLayer hypo)
