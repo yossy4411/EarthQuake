57d3c8aea38b8a413534b9bd3253aaf68fb58965
yossy4411 <yossy4411.dev@gmail.com>
2024-09-21 20:49:35 +0900

    コンバーターのリファクタリング

--- EarthQuake/Converter/Statistics/StatisticsConverter.cs
+++ EarthQuake/Converter/Statistics/StatisticsConverter.cs
@@ -1,27 +1,23 @@
 ﻿using Avalonia.Data.Converters;
 using SkiaSharp;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EarthQuake.Converter.Statistics
+namespace EarthQuake.Converter.Statistics;
+
+/// <summary>
+/// SKRectを文字列に変換するコンバーター
+/// </summary>
+public class RectangleConverter : IValueConverter
 {
-
-    public class RectangleConverter : IValueConverter
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {
-            var rect = value as SKRect? ?? SKRect.Empty;
-            return $"範囲: ({rect.Left}, {rect.Top}) - ({rect.Right}, {rect.Bottom})";
-        }
-
-        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {
-            return SKRect.Empty;
-        }
+        var rect = value as SKRect? ?? SKRect.Empty;
+        return $"範囲: ({rect.Left}, {rect.Top}) - ({rect.Right}, {rect.Bottom})";
     }
-    
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return SKRect.Empty;
+    }
 }
