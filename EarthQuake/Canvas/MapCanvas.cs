f22bf9c89660679382c8a6adc2f82654927234a0
yossy4411 <yossy4411.dev@gmail.com>
2024-09-21 18:03:13 +0900

    EarthQuake.Canvasのリファクタリング

--- EarthQuake/Canvas/MapCanvas.cs
+++ EarthQuake/Canvas/MapCanvas.cs
@@ -1,5 +1,4 @@
 ﻿using Avalonia;
-using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Media;
 using EarthQuake.Map;
@@ -7,117 +6,133 @@ using System;
 using System.Diagnostics;
 using Avalonia.Threading;
 using SkiaSharp;
-using Newtonsoft.Json.Bson;
 
-namespace EarthQuake.Canvas
+
+namespace EarthQuake.Canvas;
+
+/// <summary>
+/// マップの描画を行うCanvas
+/// </summary>
+public class MapCanvas : SkiaCanvasView
 {
-    public class MapCanvas : SkiaCanvasView
+    public class MapCanvasTranslation
     {
-        public class MapCanvasTranslation
-        {
-            public SKPoint Translate { get; set; } = new();
-            public float Scale { get; set; } = 1f;
-        }
+        public SKPoint Translate { get; set; }
+        public float Scale { get; set; } = 1f;
+    }
 
-        public MapViewController? Controller
+    public MapViewController? Controller
+    {
+        get => controller;
+        set
         {
-            get => controller;
-            set
+            controller = value;
+            if (controller is not null)
             {
-                controller = value;
-                if (controller is not null)
+                // 描画スレッドでInvalidateVisualを呼び出す。(誤ったスレッドで呼び出すと例外が発生するため)
+                controller.OnUpdated += () =>
                 {
-                    // 描画スレッドでInvalidateVisualを呼び出す。(誤ったスレッドで呼び出すと例外が発生するため)
-                    controller.OnUpdated += () =>
-                    {
-                        Dispatcher.UIThread.Post(InvalidateVisual);
-                        Debug.WriteLine($"InvalidateVisual for Canvas [{Name}]");
-                    };
-                }
+                    Dispatcher.UIThread.Post(InvalidateVisual);
+                    Debug.WriteLine($"InvalidateVisual for Canvas [{Name}]");
+                };
             }
         }
+    }
 
-        public static readonly DirectProperty<MapCanvas, MapViewController?> ControllerProperty =
-            AvaloniaProperty.RegisterDirect<MapCanvas, MapViewController?>(
-                nameof(Controller),
-                o => o.Controller,
-                (o, value) => o.Controller = value,
-                null
-                );
+    public static readonly DirectProperty<MapCanvas, MapViewController?> ControllerProperty =
+        AvaloniaProperty.RegisterDirect<MapCanvas, MapViewController?>(
+            nameof(Controller),
+            o => o.Controller,
+            (o, value) => o.Controller = value
+        );
 
-        public virtual SKColor Background => SKColors.Lavender;
-        private Point _scrollOffset;
-        private protected SKPoint Offset => Translate + Center;
+    private protected static SKColor Background => SKColors.Lavender;
+    private Point _scrollOffset;
+    private protected SKPoint Offset => Translate + Center;
 
 
-        public SKPoint Center => new((float)Bounds.Width / 2, (float)Bounds.Height / 2);
-        private protected SKPoint Translate { get => Translation.Translate; set => Translation.Translate = value; }
-        private protected float Scale { get => Translation.Scale; set => Translation.Scale = value; }
+    private SKPoint Center => new((float)Bounds.Width / 2, (float)Bounds.Height / 2);
 
-        public MapCanvasTranslation Translation { get; set; } = new();
-        public static readonly DirectProperty<MapCanvas, MapCanvasTranslation> TranslationProperty =
-            AvaloniaProperty.RegisterDirect<MapCanvas, MapCanvasTranslation>(
-                nameof(Translation),
-                o => o.Translation,
-                (o, value) => o.Translation = value
+    private SKPoint Translate
+    {
+        get => Translation.Translate;
+        set => Translation.Translate = value;
+    }
 
-                );
-        private protected bool pressed;
-        private MapViewController? controller;
+    private protected float Scale
+    {
+        get => Translation.Scale;
+        private set => Translation.Scale = value;
+    }
 
-        public override void Render(ImmediateDrawingContext context)
+    public MapCanvasTranslation Translation { get; set; } = new();
+
+    public static readonly DirectProperty<MapCanvas, MapCanvasTranslation> TranslationProperty =
+        AvaloniaProperty.RegisterDirect<MapCanvas, MapCanvasTranslation>(
+            nameof(Translation),
+            o => o.Translation,
+            (o, value) => o.Translation = value
+        );
+
+    private protected bool Pressed;
+    private MapViewController? controller;
+
+    public override void Render(ImmediateDrawingContext context)
+    {
+        using var lease = GetSKCanvas(context);
+        if (lease is null) return;
+        var canvas = lease.SkCanvas;
+
+        SKRect clipRect = new(0, 0, (float)Bounds.Width, (float)Bounds.Height);
+        canvas.ClipRect(clipRect);
+        canvas.Clear(Background);
+        var translate = Translate + Center;
+        var region = new SKRect(-translate.X / Scale, -translate.Y / Scale,
+            (float)(-translate.X + Bounds.Width) / Scale, (float)(-translate.Y + Bounds.Height) / Scale);
+        using (new SKAutoCanvasRestore(canvas))
         {
-            using var lease = GetSKCanvas(context);
-            if (lease is null) return;
-            var canvas = lease.SkCanvas;
+            canvas.Translate(Translate + Center);
+            canvas.Scale(Scale);
 
-            SKRect clipRect = new(0, 0, (float)Bounds.Width, (float)Bounds.Height);
-            canvas.ClipRect(clipRect);
-            canvas.Clear(Background);
-            var translate = Translate + Center;
-            var region = new SKRect(-translate.X / Scale, -translate.Y / Scale, (float)(-translate.X + Bounds.Width) / Scale, (float)(-translate.Y + Bounds.Height) / Scale);
-            using (new SKAutoCanvasRestore(canvas))
-            {
-                canvas.Translate(Translate + Center);
-                canvas.Scale(Scale);
-
-                Controller?.Render(canvas, Scale, region);
-            }
-
-            
+            Controller?.Render(canvas, Scale, region);
         }
+    }
 
-        protected override void OnPointerPressed(PointerPressedEventArgs e)
+    protected override void OnPointerPressed(PointerPressedEventArgs e)
+    {
+        _scrollOffset = e.GetPosition(this);
+        Pressed = e.GetCurrentPoint(this).Properties.IsLeftButtonPressed;
+        base.OnPointerPressed(e);
+    }
+
+    protected override void OnPointerMoved(PointerEventArgs e)
+    {
+        if (Pressed)
         {
+            var point = _scrollOffset - e.GetPosition(this);
             _scrollOffset = e.GetPosition(this);
-            pressed = e.GetCurrentPoint(this).Properties.IsLeftButtonPressed;
-            base.OnPointerPressed(e);
-        }
-        protected override void OnPointerMoved(PointerEventArgs e)
-        {
-            if (pressed)
-            {
-                var point = _scrollOffset - e.GetPosition(this);
-                _scrollOffset = e.GetPosition(this);
-                Translate = new(Translate.X - (float)point.X, Translate.Y - (float)point.Y);
-                InvalidateVisual();
-            }
-            base.OnPointerMoved(e);
-        }
-        protected override void OnPointerReleased(PointerReleasedEventArgs e)
-        {
-            pressed = false;
-            base.OnPointerReleased(e);
+            Translate = new SKPoint(Translate.X - (float)point.X, Translate.Y - (float)point.Y);
+            InvalidateVisual();
         }
 
-        protected override void OnPointerWheelChanged(PointerWheelEventArgs e)
-        {
-            var point = e.GetPosition(this);
-            var zoomDelta = (float)Math.Pow(1.2f, e.Delta.Y);
-            Scale *= zoomDelta;
-            Translate = new(Translate.X + Translate.X * (zoomDelta - 1) - ((float)point.X - Center.X) * (zoomDelta - 1), Translate.Y + Translate.Y * (zoomDelta - 1) - ((float)point.Y - Center.Y) * (zoomDelta - 1));
-            InvalidateVisual();
-            base.OnPointerWheelChanged(e);
-        }
+        base.OnPointerMoved(e);
+    }
+
+    protected override void OnPointerReleased(PointerReleasedEventArgs e)
+    {
+        Pressed = false;
+        base.OnPointerReleased(e);
+    }
+
+    protected override void OnPointerWheelChanged(PointerWheelEventArgs e)
+    {
+        var point = e.GetPosition(this);
+        var zoomDelta = (float)Math.Pow(1.2f, e.Delta.Y);
+        Scale *= zoomDelta;
+        Translate = new SKPoint(
+            Translate.X + Translate.X * (zoomDelta - 1) - ((float)point.X - Center.X) * (zoomDelta - 1),
+            Translate.Y + Translate.Y * (zoomDelta - 1) - ((float)point.Y - Center.Y) * (zoomDelta - 1));
+        InvalidateVisual();
+        base.OnPointerWheelChanged(e);
     }
 }
--- EarthQuake/Canvas/SelectableCanvas.cs
+++ EarthQuake/Canvas/SelectableCanvas.cs
@@ -1,108 +1,113 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Input;
+﻿using Avalonia.Input;
 using Avalonia.Media;
 using Avalonia.Platform;
 using Avalonia.Skia;
-using HarfBuzzSharp;
 using SkiaSharp;
 using System;
 
-namespace EarthQuake.Canvas
+namespace EarthQuake.Canvas;
+
+public class SelectionEventArgs(SKRect selected) : EventArgs
 {
-    public class SelectionEventArgs(SKRect selected) : EventArgs
+    public SKRect Selected { get; } = selected;
+}
+
+/// <summary>
+/// 選択可能なキャンバス
+/// </summary>
+public class SelectableCanvas : MapCanvas
+{
+    private bool selecting;
+    private SKRect Selected { get; set; }
+    private float _startX;
+    private float _startY;
+    private float _endX;
+    private float _endY;
+    public event EventHandler<SelectionEventArgs>? OnSelected;
+
+    protected override void OnPointerPressed(PointerPressedEventArgs e)
     {
-        public SKRect Selected { get; } = selected;
+        base.OnPointerPressed(e);
+        selecting = e.GetCurrentPoint(this).Properties.IsRightButtonPressed;
+        if (!selecting) return;
+        var point = e.GetPosition(this);
+        TranslateBack((float)point.X, (float)point.Y, out var x, out var y);
+        _endX = _startX = x;
+        _endY = _startY = y;
+        Selected = SKRect.Empty;
     }
-    public class SelectableCanvas : MapCanvas
+
+    protected override void OnPointerMoved(PointerEventArgs e)
     {
+        base.OnPointerMoved(e);
+        if (!selecting) return;
+        var point = e.GetPosition(this);
+        TranslateBack((float)point.X, (float)point.Y, out var x, out var y);
+        _endX = x;
+        _endY = y;
+        var left = _startX;
+        var top = _startY;
+        var right = _endX;
+        var bottom = _endY;
+        Selected = new(Math.Min(left, right), Math.Min(top, bottom), Math.Max(right, left), Math.Max(bottom, top));
+        if (!Pressed) InvalidateVisual();
+    }
 
-        private bool selecting = false;
-        public SKRect Selected { get; set; }
-        private float _startX;
-        private float _startY;
-        private float _endX;
-        private float _endY;
-        public event EventHandler<SelectionEventArgs>? OnSelected;
-        protected override void OnPointerPressed(PointerPressedEventArgs e)
+    protected override void OnPointerReleased(PointerReleasedEventArgs e)
+    {
+        base.OnPointerReleased(e);
+        InvalidateVisual();
+        if (Selected is { Width: > 2, Height: > 2 } && selecting)
+            OnSelected?.Invoke(this, new SelectionEventArgs(Selected));
+        selecting = false;
+    }
+
+    private void TranslateBack(float x, float y, out float left, out float top)
+    {
+        left = (x - Offset.X) / Scale;
+        top = (y - Offset.Y) / Scale;
+    }
+
+    public override void Render(ImmediateDrawingContext context)
+    {
+        if (!context.TryGetFeature<ISkiaSharpApiLeaseFeature>(out var feature))
         {
-
-            base.OnPointerPressed(e);
-            if (selecting = e.GetCurrentPoint(this).Properties.IsRightButtonPressed)
-            {
-                var point = e.GetPosition(this);
-                TranslateBack((float)point.X, (float)point.Y, out var x, out var y);
-                _endX = _startX = x;
-                _endY = _startY = y;
-                Selected = SKRect.Empty;
-            }
+            return;
         }
-        protected override void OnPointerMoved(PointerEventArgs e)
+
+        using var lease = feature.Lease();
+        var canvas = lease.SkCanvas;
+        SKRect clipRect = new(0, 0, (float)Bounds.Width, (float)Bounds.Height);
+        canvas.ClipRect(clipRect);
+        canvas.Clear(Background);
+        var region = new SKRect(-Offset.X / Scale, -Offset.Y / Scale, (float)(-Offset.X + Bounds.Width) / Scale,
+            (float)(-Offset.Y + Bounds.Height) / Scale);
+        using (new SKAutoCanvasRestore(canvas))
         {
-            
-            base.OnPointerMoved(e);
+            canvas.Translate(Offset);
+            canvas.Scale(Scale);
+            Controller?.Render(canvas, Scale, region);
+        }
+
+        using (new SKAutoCanvasRestore(canvas))
+        {
+            canvas.Translate(Offset);
+            canvas.Scale(Scale);
             if (selecting)
             {
-                var point = e.GetPosition(this);
-                TranslateBack((float)point.X, (float)point.Y, out var x, out var y);
-                _endX = x;
-                _endY = y;
-                var left = _startX;
-                var top = _startY;
-                var right = _endX;
-                var bottom = _endY;
-                Selected = new(Math.Min(left, right), Math.Min(top, bottom), Math.Max(right, left), Math.Max(bottom, top));
-                if (!pressed) InvalidateVisual();
+                using SKPaint paint = new();
+                paint.StrokeWidth = 2;
+                paint.PathEffect = SKPathEffect.CreateDash([7, 3], 0);
+                paint.Style = SKPaintStyle.Stroke;
+                canvas.DrawRect(Selected, paint);
             }
-        }
-        protected override void OnPointerReleased(PointerReleasedEventArgs e)
-        {
-            
-            base.OnPointerReleased(e);
-            InvalidateVisual();
-            if (Selected.Width > 2 && Selected.Height > 2 && selecting)
-                OnSelected?.Invoke(this, new(Selected));
-            selecting = false;
-        }
-        private void TranslateBack(float x, float y, out float left, out float top)
-        {
-            left = (x - Offset.X) / Scale;
-            top = (y - Offset.Y) / Scale;
-        }
-        public override void Render(ImmediateDrawingContext context)
-        {
-            if (!context.TryGetFeature<ISkiaSharpApiLeaseFeature>(out var feature)) { return; }
-            using var lease = feature.Lease();
-            var canvas = lease.SkCanvas;
-            SKRect clipRect = new(0, 0, (float)Bounds.Width, (float)Bounds.Height);
-            canvas.ClipRect(clipRect);
-            canvas.Clear(SKColors.LightBlue);
-            var region = new SKRect(-Offset.X / Scale, -Offset.Y / Scale, (float)(-Offset.X + Bounds.Width) / Scale, (float)(-Offset.Y + Bounds.Height) / Scale);
-            using (new SKAutoCanvasRestore(canvas))
+            else
             {
-
-                canvas.Translate(Offset);
-                canvas.Scale(Scale);
-                Controller?.Render(canvas, Scale, region);
+                using SKPaint paint = new();
+                paint.Color = SKColors.Coral.WithAlpha(128);
+                paint.Style = SKPaintStyle.Fill;
+                canvas.DrawRect(Selected, paint);
             }
-            using (new SKAutoCanvasRestore(canvas))
-            {
-                canvas.Translate(Offset);
-                canvas.Scale(Scale);
-                if (selecting)
-                {
-                    using SKPaint paint = new() { StrokeWidth = 2, PathEffect = SKPathEffect.CreateDash([7,3], 0), Style = SKPaintStyle.Stroke };
-                    canvas.DrawRect(Selected, paint);
-                }
-                else
-                {
-                    using SKPaint paint = new() { Color = SKColors.Coral.WithAlpha(128), Style = SKPaintStyle.Fill };
-                    canvas.DrawRect(Selected, paint);
-                }
-                
-            }
-           
-            
         }
     }
 }
