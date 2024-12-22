d044fe6c98e3c23fd694f7dde874a649d499fac5
yossy4411 <yossy4411.dev@gmail.com>
2024-12-22 15:03:36 +0900

    マージ時に発生した競合や問題を修正

---
+++ src/EarthQuake/Controls/EEWPanel.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using EarthQuake.Core.EarthQuakes.OGSP;
+
+namespace EarthQuake.Controls;
+
+public class EEWPanel : TemplatedControl
+{
+    public static readonly StyledProperty<EEW> EEWProperty = AvaloniaProperty.Register<EEWPanel, EEW>(
+        nameof(EEW));
+
+    public EEW EEW
+    {
+        get => GetValue(EEWProperty);
+        set => SetValue(EEWProperty, value);
+    }
+}
---
+++ src/EarthQuake/Converter/EpochTimeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace EarthQuake.Converter;
+
+/// <summary>
+/// エポック時間を変換するコンバーター
+/// </summary>
+public class EpochTimeConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not long epoch) return null;
+        return DateTimeOffset.FromUnixTimeMilliseconds(epoch).LocalDateTime.ToString("HH:mm:ss");
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
