b636eae3c494d1afbf5bee98d4be2d8eae291ec1
yossy4411 <yossy4411.dev@gmail.com>
2024-12-22 14:08:59 +0900

    "ディレクトリ構造を分割"をcherry-pick

---
+++ src/EarthQuake/Converter/Statistics/StatisticsConverter.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Data.Converters;
+using SkiaSharp;
+using System;
+using System.Globalization;
+
+namespace EarthQuake.Converter.Statistics;
+
+/// <summary>
+/// SKRectを文字列に変換するコンバーター
+/// </summary>
+public class RectangleConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var rect = value as SKRect? ?? SKRect.Empty;
+        return $"範囲: ({rect.Left}, {rect.Top}) - ({rect.Right}, {rect.Bottom})";
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return SKRect.Empty;
+    }
+}
