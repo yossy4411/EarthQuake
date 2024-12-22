44956e17623f55ad12a19f357eb47ffbf5db0a41
yossy4411 <yossy4411.dev@gmail.com>
2024-12-22 21:55:29 +0900

    オフセットの分ずらす

--- src/EarthQuake/Canvas/Statistics/EpicenterPlot.cs
+++ src/EarthQuake/Canvas/Statistics/EpicenterPlot.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using EarthQuake.Core.GeoJson;
 using EarthQuake.Map.Layers;
 using SkiaSharp;
 
@@ -7,8 +9,12 @@ namespace EarthQuake.Canvas.Statistics;
 
 public class EpicenterPlot : StatisticsCanvas
 {
+    private readonly List<(Epicenters.Epicenter, SKPoint)> cache = [];
+    private float _minX, _maxY, _rangeX, _rangeY;
+    private float _maxDepth;
     private protected override void Render(SKCanvas canvas)
     {
+        cache.Clear();
         using SKPaint paint = new();
         paint.Color = SKColors.Gray;
         paint.Typeface = MapLayer.Font;
@@ -17,35 +23,44 @@ public class EpicenterPlot : StatisticsCanvas
 
         var height = (float)Bounds.Height;
         var width = (float)Bounds.Width;
+        var offsetX = (float)Bounds.Left;
+        var offsetY = (float)Bounds.Top;
         CalculateOffset(Epicenters.Min(x => x.Geometry.Coordinates[0]),
                         Epicenters.Max(x => x.Geometry.Coordinates[0]), width - 50, 50, out var deltaX, out var minX,
                         out var maxX, out var count);
+        
         for (var i = 0; i <= count; i ++)
         {
-            var x = i * (width - 70) / count + 20;
-            canvas.DrawLine(x, 0, x, height, paint);
-            canvas.DrawText($"E{i * deltaX + minX:F2}", x, height - 6, paint);
+            var x = offsetX + i * (width - 70) / count + 20;
+            canvas.DrawLine(x, offsetY, x, offsetY + height, paint);
+            canvas.DrawText($"E{i * deltaX + minX:F2}", x, offsetY + height - 6, paint);
         }
         var rangeX = maxX - minX;
+        _minX = minX;
+        _rangeX = rangeX;
+        
         CalculateOffset(Epicenters.Min(x => x.Geometry.Coordinates[1]),
-            Epicenters.Max(x => x.Geometry.Coordinates[1]), height - 50, 50, out var deltaY, out var minY,
+                        Epicenters.Max(x => x.Geometry.Coordinates[1]), height - 50, 50, out var deltaY, out var minY,
             out var maxY, out count);
         for (var i = 0; i <= count; i ++)
         {
-            var y = i * (height - 70) / count + 50;
-            canvas.DrawLine(0, y, width, y, paint);
-            canvas.DrawText($"N{i * deltaY + minY:F2}", 0, y, paint);
+            var y = offsetY + i * (height - 70) / count + 50;
+            canvas.DrawLine(offsetX, y, offsetX + width, y, paint);
+            canvas.DrawText($"N{maxY - i * deltaY:F2}", offsetX, y, paint);
         }
         var rangeY = maxY - minY;
+        _maxY = maxY;
+        _rangeY = rangeY;
         
         paint.StrokeWidth = 1;
-        var dMax = Epicenters.Select(x => x.Properties.Dep ?? 0).Max();
+        var maxDepth = MathF.Ceiling(Epicenters.Select(x => x.Properties.Dep ?? 0).Max());
+        _maxDepth = maxDepth;
         foreach (var item in Epicenters)
         {
-            var x = (item.Geometry.Coordinates[0] - minX) * (width - 70) / rangeX + 20;
-            var y = (maxY - item.Geometry.Coordinates[1]) * (height - 70) / rangeY + 50;
-            byte alpha = 255; // デバッグのためにコメントアウト
-            /*item.Properties.Mag switch
+            var x = offsetX + (item.Geometry.Coordinates[0] - minX) / rangeX * (width - 70) + 20;
+            var y = offsetY + (maxY - item.Geometry.Coordinates[1]) / rangeY * (height - 70) + 50;
+            cache.Add((item, new SKPoint(x, y)));
+            byte alpha = item.Properties.Mag switch
             {
                 >= 5 => 255,
                 >= 4 => 200,
@@ -53,10 +68,10 @@ public class EpicenterPlot : StatisticsCanvas
                 >= 2 => 100,
                 >= 1 => 50,
                 _ => 25
-            };*/
+            };
             paint.Color = SKColors.Pink.WithAlpha(alpha);
-            canvas.DrawPoint(x, (item.Properties.Dep ?? 0) / dMax * 50, paint);
-            canvas.DrawPoint(width - 50 + (item.Properties.Dep ?? 0) / dMax * 50, y, paint);
+            canvas.DrawPoint(x, offsetY + (item.Properties.Dep ?? 0) / maxDepth * 50, paint);
+            canvas.DrawPoint(width - 50 - offsetX + (item.Properties.Dep ?? 0) / maxDepth * 50, y, paint);
             canvas.DrawPoint(x, y, paint);
         }
     }
@@ -84,6 +99,70 @@ public class EpicenterPlot : StatisticsCanvas
         delta = Math.Abs(delta1 - preferredDelta) < Math.Abs(delta2 - preferredDelta) ? delta1 : delta2;
         start = (float)Math.Floor(min / delta) * delta;
         end = (float)Math.Ceiling(max / delta) * delta;
-        count = (int)((end - start) / delta);
+        count = (int)Math.Round((end - start) / delta);
+    }
+
+    private protected override void RenderOverlay(SKCanvas canvas, SKPoint mousePoint)
+    {
+        base.RenderOverlay(canvas, mousePoint);
+        using SKPaint paint = new();
+        paint.Color = SKColors.Gray;
+        paint.Typeface = MapLayer.Font;
+        paint.TextSize = 6;
+        paint.IsAntialias = true;
+        var height = (float)Bounds.Height;
+        var width = (float)Bounds.Width;
+        var offsetX = (float)Bounds.Left;
+        var offsetY = (float)Bounds.Top;
+        
+        // 一番マウスに近い点を探す
+        var (item, point) = cache.OrderBy(tuple => SKPoint.DistanceSquared(tuple.Item2, mousePoint)).First();
+        if (SKPoint.Distance(point, mousePoint) > 3)
+        {
+            paint.PathEffect = SKPathEffect.CreateDash([5, 5], 0);
+            canvas.DrawLine(0, mousePoint.Y, width, mousePoint.Y, paint);
+            canvas.DrawLine(mousePoint.X, 0, mousePoint.X, height, paint);
+            paint.Color = SKColors.White;
+            canvas.DrawText(
+                mousePoint.Y < 50 + offsetY
+                    ? $"D{_maxDepth - (50 + offsetY - mousePoint.Y) / 50 * _maxDepth:F2}"
+                    : $"N{_maxY - (mousePoint.Y - 50 - offsetY) / (height - 70) * _rangeY:F4}", offsetX, mousePoint.Y, paint);
+
+            if (mousePoint.X > width - 50 + offsetX)
+            {
+                canvas.DrawText(
+                    $"D{(mousePoint.X - width + 50 + offsetX) / 50 * _maxDepth:00.0}",
+                    mousePoint.X - 17, height - 6, paint);
+            }
+            else
+            {
+                canvas.DrawText(
+                    $"E{(mousePoint.X - 20 - offsetX) / (width - 70) * _rangeX + _minX:F4}",
+                    mousePoint.X, height - 6, paint);
+            }
+
+            return;
+        }
+        paint.Color = SKColors.DodgerBlue.WithAlpha(200);
+        paint.TextSize = 7;
+        paint.TextAlign = SKTextAlign.Left;
+        var text = $"N{item.Geometry.Coordinates[0]:0.000} E{item.Geometry.Coordinates[1]:0.000}";
+        var textWidth = paint.MeasureText(text);
+        var offset = Math.Min(5, width - mousePoint.X - textWidth - 10);
+        canvas.DrawRect(mousePoint.X + offset, mousePoint.Y - 20, textWidth + 10, 20, paint);
+        paint.Color = SKColors.White;
+
+        canvas.DrawText($"M{item.Properties.Mag:F1} D{item.Properties.Dep:F1}km", mousePoint.X + 5 + offset, mousePoint.Y - 3, paint);
+        canvas.DrawText(text, mousePoint.X + 5 + offset, mousePoint.Y - 10, paint);
+        
+        paint.Color = SKColors.Gray;
+        paint.PathEffect = SKPathEffect.CreateDash([5, 5], 0);
+        canvas.DrawLine(0, point.Y, width, point.Y, paint);
+        canvas.DrawLine(point.X, 0, point.X, height, paint);
+
+        paint.PathEffect = null;
+        paint.Color = SKColors.Red;
+        paint.StrokeWidth = 2;
+        canvas.DrawPoint(point, paint);
     }
 }
--- src/EarthQuake/Canvas/Statistics/StatisticsCanvas.cs
+++ src/EarthQuake/Canvas/Statistics/StatisticsCanvas.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Avalonia;
+using Avalonia.Input;
 using Avalonia.Media;
+using Avalonia.Skia;
 using Avalonia.Threading;
 using EarthQuake.Core.GeoJson;
 using EarthQuake.Map.Layers;
@@ -14,11 +15,33 @@ namespace EarthQuake.Canvas.Statistics;
 /// </summary>
 public abstract class StatisticsCanvas : SkiaCanvasView
 {
-    
     private SKPicture? picture;
     
     private protected List<Epicenters.Epicenter> Epicenters = [];
     
+    private SKPoint _mousePoint;
+    
+    private bool _isMouseOver; // マウスが乗っているか
+    
+    protected override void OnPointerMoved(PointerEventArgs e)
+    {
+        base.OnPointerMoved(e);
+        _mousePoint = e.GetPosition(this).ToSKPoint();
+        InvalidateVisual();
+    }
+
+    protected override void OnPointerEntered(PointerEventArgs e)
+    {
+        base.OnPointerEntered(e);
+        _isMouseOver = true;
+    }
+    
+    protected override void OnPointerExited(PointerEventArgs e)
+    {
+        base.OnPointerExited(e);
+        _isMouseOver = false;
+    }
+
     public void SetEpicenters(List<Epicenters.Epicenter> epicenters)
     {
         Epicenters = epicenters;
@@ -41,6 +64,8 @@ public abstract class StatisticsCanvas : SkiaCanvasView
         using var lease = GetSKCanvas(context);
         if (lease is null) return;
         var canvas = lease.SkCanvas;
+        var rect = Bounds.ToSKRect();
+        canvas.ClipRect(rect);
         canvas.Clear(SKColors.Black);
         
         if (Epicenters.Count == 0)
@@ -57,34 +82,49 @@ public abstract class StatisticsCanvas : SkiaCanvasView
         if (picture is not null)
         {
             canvas.DrawPicture(picture);
-            return;
         }
-        
-        using var paint1 = new SKPaint();
-        paint1.Color = SKColors.White;
-        paint1.TextSize = 15;
-        paint1.TextAlign = SKTextAlign.Center;
-        paint1.Typeface = MapLayer.Font;
-        paint1.IsAntialias = true;
-        canvas.DrawText("読込中", (float)(Bounds.Width / 2), (float)(Bounds.Height / 2), paint1);
-        
-        // 別スレッドでキャッシュを溜めてユーザーの待ち時間を減らす
-        Task.Run(() =>
+        else
         {
-            using var recorder = new SKPictureRecorder();
-            var rect = new SKRect(0, 0, (float)Bounds.Width, (float)Bounds.Height);
-            var recordCanvas = recorder.BeginRecording(rect);
-            Render(recordCanvas);
-            picture?.Dispose();
-            picture = recorder.EndRecording();
+            using var paint1 = new SKPaint();
+            paint1.Color = SKColors.White;
+            paint1.TextSize = 15;
+            paint1.TextAlign = SKTextAlign.Center;
+            paint1.Typeface = MapLayer.Font;
+            paint1.IsAntialias = true;
+            canvas.DrawText("読込中", (float)(Bounds.Width / 2), (float)(Bounds.Height / 2), paint1);
 
-            Dispatcher.UIThread.Invoke(InvalidateVisual);
-        });
-        
-        
-        
+            // 別スレッドでキャッシュを溜めてユーザーの待ち時間を減らす
+            Task.Run(() =>
+            {
+                using var recorder = new SKPictureRecorder();
+                var recordCanvas = recorder.BeginRecording(Bounds.ToSKRect());
+                Render(recordCanvas);
+                picture?.Dispose();
+                picture = recorder.EndRecording();
+
+                Dispatcher.UIThread.Invoke(InvalidateVisual);
+            });
+        }
+
+        if (_isMouseOver)
+        {
+            RenderOverlay(canvas, _mousePoint);
+        }
+    }
+    
+    /// <summary>
+    /// メイン描画処理
+    /// </summary>
+    /// <param name="canvas">キャンバス</param>
+    private protected abstract void Render(SKCanvas canvas);
+
+    /// <summary>
+    /// オーバーレイ描画処理
+    /// </summary>
+    /// <param name="canvas">キャンバス</param>
+    /// <param name="mousePoint">マウスの位置</param>
+    private protected virtual void RenderOverlay(SKCanvas canvas, SKPoint mousePoint)
+    {
         
     }
-
-    private protected abstract void Render(SKCanvas canvas);
 }
